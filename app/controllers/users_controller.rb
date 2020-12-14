40b42202967656283b9c58f6fc22fbd27cabc033
EpsilonPoint0 <noahguzik123@gmail.com>
2020-12-14 16:01:08 -0500

    halfway done with chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
   attr_accessor :name, :email, :password, :password_confirmation
   def new
+    @user = User.new
   end
 
   def show
     # Find user by id
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user]) # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
 end
