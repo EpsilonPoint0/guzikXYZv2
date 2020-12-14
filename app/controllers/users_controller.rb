eb09a166f5ac285d2d8c82360d1855259e0fc488
EpsilonPoint0 <noahguzik123@gmail.com>
2020-12-14 15:12:18 -0500

    Temp to reboot

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   attr_accessor :name, :email, :password, :password_confirmation
   def new
   end
+
+  def show
+    # Find user by id
+    @user = User.find(params[:id])
+  end
 end
